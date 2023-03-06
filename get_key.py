3d60291f4276818c64b709d569a74579fc7a3018
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-06 10:10:12 +0530

    fixed some bugs about Runtime error
    Fixed a runtime error that would happen if get_key() function ran twice

--- get_key.py
+++ get_key.py
@@ -5,6 +5,7 @@ def on_press(key):
 
 def on_release(key):
     global key_input
+    
     if key:
         try:
             key_input = key.char
@@ -14,15 +15,25 @@ def on_release(key):
             key_input = key.name
             keyboard_listener.stop()
 
-keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+keyboard_listener = None
 key_input = None
 
 def get_key(prompt_string):
     global key_input
+    global keyboard_listener
     
-    print("Key to toggle autoclicker: ", end="")
+    if keyboard_listener is None or not keyboard_listener.running:
+        keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release, daemon=True)
+        key_input = None
+        keyboard_listener.start()
     
-    keyboard_listener.start()
+    key_input = None
+    print(prompt_string, end="")
+    
+    while key_input is None:
+        pass
+    
+    keyboard_listener.stop()
     keyboard_listener.join()
     
     print(key_input)
