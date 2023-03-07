9f94b25e458f856e808306b63ae8ea035a2a8028
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-07 21:44:33 +0530

    added comments to document what each line is supposed to do

--- mouse_clicker.py
+++ mouse_clicker.py
@@ -1,8 +1,12 @@
+# import necessary modules
+from pynput.mouse import Controller, Button
 import time
 import threading
 
+# define AutoClicker class and make is inherit Thread class
 class AutoClicker(threading.Thread):
-    def __init__(self, mouse, delay, button):
+    # define init function to create all class (self) variables
+    def __init__(self, mouse: Controller, delay: float, button: Button):
         super(AutoClicker, self).__init__()
         self.mouse = mouse
         self.delay = delay
@@ -10,20 +14,24 @@ class AutoClicker(threading.Thread):
         self.running = False
         self.program_running = True
 
+    # define function to start autoclicker
     def start_clicking(self):
         self.running = True
 
+    # define function to stop autoclicker
     def stop_clicking(self):
         self.running = False
 
+    # define function to exit autoclicker
     def exit(self):
         self.stop_clicking()
         self.program_running = False
 
+    # define function to run the autoclicker
     def run(self):
-        while self.program_running:
-            while self.running:
-                self.mouse.click(self.button)
-                time.sleep(self.delay)
-            time.sleep(0.1)
+        while self.program_running: # checks if program is running
+            while self.running: # checks if autoclicker is running
+                self.mouse.click(self.button) # clicks the mouse button that is set for autoclicking
+                time.sleep(self.delay) # delays the autoclicker by set ammount
+            time.sleep(0.1) # delays main loop so the program doesn't crash
 
