6680a58152f2581866938ed4a64007e9b5bdf5eb
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-09 14:18:29 +0530

    created a constants file for ANSI escape codes
    created constants files which contains all ANSI escape codes for text colors, text background, and text decorations.

---
+++ constants.py
@@ -0,0 +1,47 @@
+# Reset
+RESET = "\033[0m"
+
+# Normal colors
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+# Bright Colors
+BRIGHT_BLACK = "\033[30;1m"
+BRIGHT_RED = "\033[31;1m"
+BRIGHT_GREEN = "\033[32;1m"
+BRIGHT_YELLOW = "\033[33;1m"
+BRIGHT_BLUE = "\033[34;1m"
+BRIGHT_MAGENTA = "\033[35;1m"
+BRIGHT_CYAN = "\033[36;1m"
+BRIGHT_WHITE = "\033[37;1m"
+
+# Text background colors (background = B)
+B_BLACK = "\033[40m"
+B_RED = "\033[41m"
+B_GREEN = "\033[42m"
+B_YELLOW = "\033[43m"
+B_BLUE = "\033[44m"
+B_MAGENTA = "\033[45m"
+B_CYAN = "\033[46m"
+B_WHITE = "\033[47m"
+
+# Text bright background colors (bright background = BB)
+BB_BLACK = "\033[40;1m"
+BB_RED = "\033[41;1m"
+BB_GREEN = "\033[42;1m"
+BB_YELLOW = "\033[43;1m"
+BB_BLUE = "\033[44;1m"
+BB_MAGENTA = "\033[45;1m"
+BB_CYAN = "\033[46;1m"
+BB_WHITE = "\033[47;1m"
+
+# Others
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+REVERSED = "\033[7m"
