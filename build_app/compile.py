1c415866a8227db1fc80cf2266a4b215c6d11fa6
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-13 13:50:11 +0530

    created script to automatically compile all py files to pyc

---
+++ build_app/compile.py
@@ -0,0 +1,19 @@
+import py_compile, time
+
+from src.colors.constants import RESET, CYAN, GREEN
+from src.colors.vtp import enable_vtp, disable_vtp
+
+files = ["main.py", "clickers/mouse_clicker.py", "clickers/keyboard_clicker.py", "prompts.py", "get_key.py", "colors/vtp.py", "colors/constants.py"]
+files_directory = "src"
+compiled_directory = "build/compiled_files"
+
+enable_vtp()
+
+for file in files:
+    py_compile.compile(f'{files_directory}/{file}', cfile=f'{compiled_directory}/{file.split(".")[0]}.pyc')
+    print(f"{CYAN}Compiled {file} -> {file.split('.')[0]}.pyc{RESET}")
+    time.sleep(0.1)
+    
+print(f"{GREEN}Compiled files can be found in '{compiled_directory}' folder{RESET}")
+
+disable_vtp()
