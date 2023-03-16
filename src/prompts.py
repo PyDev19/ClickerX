de9b7f397008f6dbaa62e1d252f3bd0aa30ba56a
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-16 19:07:52 +0530

    implemented key class and changed imports

--- src/prompts.py
+++ src/prompts.py
@@ -2,39 +2,17 @@
 from typing import Tuple
 from pynput.mouse import Button
 from configparser import ConfigParser
-import msvcrt, os
+import os
 
 # Import colors modules
-from src.keys.get_key import get_key
-from src.keys.get_mode import get_mode
+from src.key import Key
+from src.mode import get_mode
 from src.settings import config_prompt
 from src.colors.constants import RED, RESET, BLUE, YELLOW, CYAN
 
 loaded_setting = None
 button_map = {"left mouse": Button.left, "right mouse": Button.right, "middle mouse": Button.middle}
-
-# Defines function to get user input with a prompt string and returns user input string.
-def get_input(prompt: str) -> str:
-    """
-    Clears any key presses waiting to be processed and gets user input from the console and returns it as a string.
-
-    Args:
-        prompt: A string representing the prompt to display to the user.
-
-    Returns:
-        A string representing the user's input.
-    """
-    
-    # Loop to check if a key press is waiting to be processed.
-    while msvcrt.kbhit():
-        # Function to block a pending key press.
-        msvcrt.getch()
-    
-    # Get input from user by asking prompt string from function parameter.
-    user_input = input(prompt)
-    
-    # Return the user input value to the calling function.
-    return user_input
+key = Key()
 
 # Function to get user input for toggle key, exit key, delay, and button to be autoclicked.
 def prompts() -> Tuple[str, str, str, float, Button]:
@@ -72,22 +50,28 @@ def prompts() -> Tuple[str, str, str, float, Button]:
     
     if not loaded_setting:
         # Get toggle key from user by calling get_key function with a prompt message.
-        toggle_key = get_key(f"{BLUE}Key to toggle autoclicker (press any key): {RESET}")
+        toggle_key = key.get_key(f"{BLUE}Key to toggle autoclicker (press any key): {RESET}")
+        # Print the toggle_key that the user has pressed.
+        print(YELLOW + toggle_key + RESET)
         
         # Get exit key from user by calling get_key function with a prompt message.
-        exit_key = get_key(f"{BLUE}Key to exit program (press any key): {RESET}")
+        exit_key = key.get_key(f"{BLUE}Key to exit program (press any key): {RESET}")
+        # Print the exit_key that the user has pressed.
+        print(YELLOW + exit_key + RESET)
         
         if mode == "k":
             # Get key to be autoclicked from user by calling get_key function with a prompt message.
-            button = get_key(f"{BLUE}Key to be autoclicked (press any key): {RESET}")
+            button = key.get_key(f"{BLUE}Key to be autoclicked (press any key): {RESET}")
+            # Print the button that the user has pressed.
+            print(YELLOW + button + RESET)
             
             # Get delay between key presses in seconds from user by calling get_input function with a prompt message.
-            delay = float(get_input(f"{BLUE}Delay between key presses (in seconds): {YELLOW}"))
+            delay = float(key.get_input(f"{BLUE}Delay between key presses (in seconds): {YELLOW}"))
             print(f"{RESET}", end="")
             
         if mode == "m":
             # Get delay between key presses in seconds from user by calling get_input function with a prompt message.
-            delay = float(get_input(f"{BLUE}Delay between mouse clicks (in seconds): {YELLOW}"))
+            delay = float(key.get_input(f"{BLUE}Delay between mouse clicks (in seconds): {YELLOW}"))
             print(f"{RESET}", end="")
             
             # Get key to be autoclicked from user by calling get_key function with a prompt message.
