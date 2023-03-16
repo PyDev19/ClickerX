332c5ce75d470b9c4f9a428e4b126486ec3d93b5
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-16 20:54:40 +0530

    fixed path of files to be compiled

--- build_app/compile.py
+++ build_app/compile.py
@@ -3,7 +3,7 @@ import py_compile, time
 from src.colors.constants import RESET, CYAN, GREEN
 from src.colors.vtp import enable_vtp, disable_vtp
 
-files = ["main.py", "clickers/mouse_clicker.py", "clickers/keyboard_clicker.py", "prompts.py", "keys/get_key.py", "keys/get_mode.py", "colors/vtp.py", "colors/constants.py", "settings.py"]
+files = ["main.py", "classes/mouse_clicker.py", "classes/keyboard_clicker.py", "prompts.py", "classes/key.py", "mode.py", "colors/vtp.py", "colors/constants.py", "settings.py"]
 files_directory = "src"
 compiled_directory = "build/compiled_files"
 
@@ -15,5 +15,6 @@ for file in files:
     time.sleep(0.1)
     
 print(f"{GREEN}Compiled files can be found in '{compiled_directory}' folder{RESET}")
+print("\n")
 
 disable_vtp()
