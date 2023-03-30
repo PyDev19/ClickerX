cf2da3a76c832d5f5bf3a8e677d629bb3f959016
Atharva Mishra <83823608+PyDev19@users.noreply.github.com>
2023-03-30 11:38:23 +0530

    modified include files to add Xlib modules

--- build_app/setup.py
+++ build_app/setup.py
@@ -1,38 +1,45 @@
 from cx_Freeze import setup, Executable
-import glob, os
+import glob, os, pkgutil, Xlib.ext, Xlib.keysymdef
 
 dir_path = 'build/compiled_files'
 
-files = glob.glob("build/compiled_files/*.pyc")
+files = glob.glob('build/compiled_files/*.pyc')
 files = [f for f in files if not f.endswith('main.pyc')]
 
 for dir in os.listdir(dir_path):
     if os.path.isdir(os.path.join(dir_path, dir)):
         files.append(os.path.join(dir_path, dir))
 
-excludes = ["tkinter", "unittest", "email", "html", "http", "pydoc_data", "urllib", "xml", "hashlib", "socket", "ssl"]
-includes = ["queue"]
-output_dir = "build/app"
+excludes = ['tkinter', 'unittest', 'email', 'html', 'http', 'pydoc_data', 'urllib', 'xml', 'hashlib', 'ssl']
+includes = ['queue', 'pynput.keyboard._xorg', 'pynput.mouse._xorg']
+output_dir = 'build/app'
+
+xlib_ext_modules = pkgutil.iter_modules(Xlib.ext.__path__)
+xlib_keysymdef_modules = pkgutil.iter_modules(Xlib.keysymdef.__path__)
+
+for importer, modname, ispkg in xlib_ext_modules:
+    includes.append(f'Xlib.ext.{modname}')
+
+for importer, modname, ispkg in xlib_keysymdef_modules:
+    includes.append(f'Xlib.keysymdef.{modname}')
 
 build_exe_options = {
-    "include_files": files,
-    "excludes": excludes,
-    "includes": includes,
+    'include_files': files,
+    'excludes': excludes,
+    'includes': includes,
     'build_exe': output_dir
 }
 
-base = None
-
 target = Executable(
-    script="src/main.py",
-    base=base,
-    icon="icon.ico"
+    script='src/main.py',
+    base='Console',
+    icon='icon.ico'
 )
 
 setup(
-    name="PyClickerX",
-    version="3",
-    description="console based autoclicker",
-    options={"build_exe": build_exe_options},
+    name='PyClickerX',
+    version='3',
+    description='console based autoclicker',
+    options={'build_exe': build_exe_options},
     executables=[target]
 )
