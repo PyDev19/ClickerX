a811e5f747ea08733930886c8994251322a3eb8a
Atharva Mishra <atharvamishra956+1@gmail.com>
2023-12-16 12:58:51 -0600

    updated arguments for methods to match the updated header file

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,9 +1,15 @@
-#include <ui.hpp>
+#include "ui.hpp"
 #include <QQuickItem>
 
-UI::UI(QQmlEngine &engine, QObject* root_object): engine(engine), root_object(root_object), number_field_component(&engine, QUrl("qrc:qml/components/NumberField.qml")) {}
+UI::UI(QQmlEngine &engine, QObject* parent): QObject(parent), engine(engine) {}
 
-int UI::setup_ui() {
+/**
+ * @brief This function is used to setup the ui by dynamically creating qml components needed in the application
+*/
+int UI::setup_ui(QObject* _root_object) {
+    root_object = _root_object;
+    QQmlComponent number_field_component(&engine, QUrl("qrc:qml/components/NumberField.qml"));\
+
     row_layout = root_object->findChild<QObject*>("click_interval_layout");
     if (!row_layout) {
         qWarning("RowLayout not found!");
@@ -26,18 +32,22 @@ int UI::setup_ui() {
                 case 0: 
                     number_field_item->setProperty("placeholderText", "Hours");
                     number_field_item->setProperty("id", "hours");
+                    number_fields["hours"] = number_field_item;
                     break;
                 case 1:
                     number_field_item->setProperty("placeholderText", "Minutes");
                     number_field_item->setProperty("id", "minutes");
+                    number_fields["minutes"] = number_field_item;
                     break;
                 case 2:
                     number_field_item->setProperty("placeholderText", "Seconds");
                     number_field_item->setProperty("id", "seconds");
+                    number_fields["seconds"] = number_field_item;
                     break;
                 case 3:
                     number_field_item->setProperty("placeholderText", "Milliseconds");
                     number_field_item->setProperty("id", "milliseconds");
+                    number_fields["milliseconds"] = number_field_item;
                     break;
             }
         } else {
