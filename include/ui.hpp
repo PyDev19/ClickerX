36178cbe6853e30494c4a39863534b05bf8884cc
Atharva Mishra <atharvamishra956+1@gmail.com>
2023-12-16 12:57:47 -0600

    turned ui class to object so that it can be interacted with from qml

--- include/ui.hpp
+++ include/ui.hpp
@@ -3,9 +3,11 @@
 
 #include <QQmlEngine>
 #include <QQmlComponent>
+#include <QObject>
+#include <QQuickItem>
 
-
-class UI {
+class UI: public QObject {
+    Q_OBJECT
 private:
     QQmlEngine& engine;
     QObject* root_object;
@@ -13,9 +15,10 @@ private:
 
 public:
     QObject* row_layout;
+    QHash<QString, QQuickItem*> number_fields;
 
-    UI(QQmlEngine& engine, QObject* root_object);
-    int setup_ui();
+    explicit UI(QQmlEngine &engine, QObject* parent = nullptr);
+    int setup_ui(QObject* root_object);
 };
 
 #endif
